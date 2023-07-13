c240fafa3e1a0e9a1c9645beb6223bbb154cdeb4
Jake Overall <joverall22@gmail.com>
2023-07-13 01:46:08 -0600

    first steps

---
+++ app.js
@@ -0,0 +1,49 @@
+let startScreen = document.getElementById('start-screen')
+let mapScreen = document.getElementById('map-screen')
+let stage1Screen = document.getElementById('stage-1-screen')
+let stage2Screen = document.getElementById('stage-2-screen')
+let stage3Screen = document.getElementById('stage-3-screen')
+let victoryScreen = document.getElementById('victory-screen')
+let gameOverScreen = document.getElementById('game-over-screen')
+
+startScreen.classList.add('show')
+
+function hideActiveScreen() {
+  document.querySelector('.show').classList.remove('show')
+}
+
+function showStartScreen() {
+  hideActiveScreen()
+  startScreen.classList.add('show')
+}
+
+function showMap() {
+  hideActiveScreen()
+  mapScreen.classList.add('show')
+}
+
+function showStage1() {
+  hideActiveScreen()
+  stage1Screen.classList.add('show')
+}
+
+function showStage2() {
+  hideActiveScreen()
+  stage2Screen.classList.add('show')
+}
+
+function showStage3() {
+  hideActiveScreen()
+  stage3Screen.classList.add('show')
+}
+
+function showVictory() {
+  hideActiveScreen()
+  victoryScreen.classList.add('show')
+}
+
+function showGameOver() {
+  hideActiveScreen()
+  gameOverScreen.classList.add('show')
+}
+
